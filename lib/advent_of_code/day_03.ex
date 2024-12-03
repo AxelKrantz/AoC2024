4fca88c22d2bc93960b2c5b6500d91062d103705
AxelKrantz <axel.krantz@gmail.com>
2024-12-03 07:40:46 +0100

    d3p1

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,5 +1,16 @@
 defmodule AdventOfCode.Day03 do
   def part1(_args) do
+    input = AdventOfCode.Input.get!(3)
+    regex = ~r/mul\(\d+,\d+\)/
+    secex = ~r/\d+/
+
+    Regex.scan(regex, input)
+    |> Enum.map(fn [match] ->
+      numbers = Regex.scan(~r/\d+/, match) |> List.flatten()
+      [a, b] = Enum.map(numbers, &String.to_integer/1)
+      a * b
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
