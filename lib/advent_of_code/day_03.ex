1e7caca2f5a18e4f7b3b69e8caebc6f50c973223
AxelKrantz <axel.krantz@gmail.com>
2024-12-03 21:43:23 +0100

    p2 not working

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,8 +1,11 @@
 defmodule AdventOfCode.Day03 do
   def part1(_args) do
     input = AdventOfCode.Input.get!(3)
+    scan(input)
+  end
+
+  def scan(input) do
     regex = ~r/mul\(\d+,\d+\)/
-    secex = ~r/\d+/
 
     Regex.scan(regex, input)
     |> Enum.map(fn [match] ->
@@ -14,5 +17,44 @@ defmodule AdventOfCode.Day03 do
   end
 
   def part2(_args) do
+    input = AdventOfCode.Input.get!(3)
+    prefix = "do()"
+
+    input =
+      prefix <> input
+
+    validate_expression(input, 0)
+  end
+
+  def validate_expression(string, sum) do
+    IO.puts("Current sum: #{sum}")
+
+    case Regex.run(~r/(.*?mul\(\d{1,3},\d{1,3}\))(.*)/, string, capture: :all_but_first) do
+      result when is_list(result) ->
+        [matched, leftover] = result
+
+        conditionals =
+          Regex.scan(~r/do\(\)|don't\(\)/, matched)
+          |> Enum.map(&hd/1)
+
+        if last_is_do?(conditionals) do
+          IO.inspect("Conditionals: #{conditionals}")
+          new_sum = scan(matched) + sum
+          validate_expression(leftover, new_sum)
+        else
+          validate_expression(leftover, sum)
+        end
+
+      nil ->
+        IO.puts("No match found.")
+        sum
+    end
+  end
+
+  def last_is_do?(list) do
+    case List.last(list) do
+      "do()" -> true
+      _ -> false
+    end
   end
 end
